676b4af602a3bc4a53559ad023619fa549eb6eb8
Sachin Tripathi <sachintripathis84@gmail.com>
2019-10-05 12:24:14 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -25,7 +25,7 @@ def convert(amount, desired_currency, conversion_rates):
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        amount = sys.argv[1].upper()
+        amount = sys.argv[1]
         base = sys.argv[2].upper()
         to_currency = sys.argv[3].upper()
 
