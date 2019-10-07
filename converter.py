0daa3ce20557c7a01f5bb6511b7633735afa2ab3
unknown <scapogo@gmail.com>
2019-10-07 19:39:13 +0200

    argparse added

--- converter.py
+++ converter.py
@@ -2,7 +2,15 @@
 import sys
 import json
 import requests
+import argparse
 
+parser = argparse.ArgumentParser(description="Currency ceverter tool")
+parser.add_argument('-a', dest='amount', help='Amount we want to convert')
+parser.add_argument('-b', dest='base', help='Base currency we are converting from')
+parser.add_argument('-t', dest='to_currency', help='Target currency we are converting to')
+parser.add_argument('-d', dest='date', help='Date from which we want conversion rates')
+
+args = parser.parse_args()
 
 def getRates(base_currency, date='latest'):
     url = f'https://api.ratesapi.io/api/{date}?base={base_currency}'
@@ -32,34 +40,27 @@ def convert(amount, desired_currency, conversion_rates):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        amount = sys.argv[1]
-        base = sys.argv[2].upper()
-        to_currency = sys.argv[3].upper()
-        date = sys.argv[4]
-
-        rates = getRates(base, date)
-        result = convert(amount, to_currency, rates)
-        print(result)
-
-    elif len(sys.argv) == 4:
-        amount = sys.argv[1]
-        base = sys.argv[2].upper()
-        to_currency = sys.argv[3].upper()
-
-        rates = getRates(base)
-        result = convert(amount, to_currency, rates)
-        print(result)
-
-    elif len(sys.argv) == 2:
-        if sys.argv[1].upper() == "HELP":
-            print(available_currencies)
-
-    else:
+    if args.amount == None:
         amount = int(input("[*]Enter amount > "))
+    else:
+        amount = int(args.amount)
+
+    if args.base == None:
         base = input("[*]Enter Base Currency > ").upper()
+    else:
+        base = args.base.upper()
+
+    if args.to_currency == None:
         to_currency = input(
             "[*]Enter the currency you need to convert in > ").upper()
+    else:
+        to_currency = args.to_currency.upper()
+
+    if args.date == None:
         rates = getRates(base)
-        result = convert(amount, to_currency, rates)
-        print(result)
+    else:
+        rates = getRates(base, args.date)
+    
+    result = convert(amount, to_currency, rates)
+    
+    print(result)
