6a4fea4a00b4770d14110e5435be95f01d27548b
Wiktor Matuszewski <pywkm@wukaem.pl>
2019-10-25 00:59:28 +0200

    Remove unused imports and function; redact strings; pep8 fixes

--- converter.py
+++ converter.py
@@ -1,18 +1,16 @@
-
-import sys
-import json
 import requests
 import argparse
 
-parser = argparse.ArgumentParser(description="Currency ceverter tool")
-parser.add_argument('-a', dest='amount', help='Amount we want to convert')
-parser.add_argument('-b', dest='base', help='Base currency we are converting from')
-parser.add_argument('-t', dest='to_currency', help='Target currency we are converting to')
-parser.add_argument('-d', dest='date', help='Date from which we want conversion rates')
+parser = argparse.ArgumentParser(description="Currency converter tool")
+parser.add_argument('-a', dest='amount', help='Amount you want to convert')
+parser.add_argument('-b', dest='base', help='Base currency you are converting from')
+parser.add_argument('-t', dest='to_currency', help='Target currency you are converting to')
+parser.add_argument('-d', dest='date', help='Date from which you want conversion rates')
 
 args = parser.parse_args()
 
-def getRates(base_currency, date='latest'):
+
+def get_rates(base_currency, date='latest'):
     url = f'https://api.ratesapi.io/api/{date}?base={base_currency}'
     api_response = requests.get(url)
     rates_json = api_response.json()
@@ -25,11 +23,6 @@ def getRates(base_currency, date='latest'):
         raise ValueError(error)
 
 
-def available_currencies(rates):
-    currencies = list(rates)
-    return currencies
-
-
 def convert(amount, desired_currency, conversion_rates):
     if desired_currency in conversion_rates:
         converted_amount = float(
@@ -40,26 +33,26 @@ def convert(amount, desired_currency, conversion_rates):
 
 
 if __name__ == "__main__":
-    if args.amount == None:
+    if not args.amount:
         amount = int(input("[*]Enter amount > "))
     else:
         amount = int(args.amount)
 
-    if args.base == None:
+    if not args.base:
         base = input("[*]Enter Base Currency > ").upper()
     else:
         base = args.base.upper()
 
-    if args.to_currency == None:
+    if not args.to_currency:
         to_currency = input(
             "[*]Enter the currency you need to convert in > ").upper()
     else:
         to_currency = args.to_currency.upper()
 
-    if args.date == None:
-        rates = getRates(base)
+    if not args.date:
+        rates = get_rates(base)
     else:
-        rates = getRates(base, args.date)
+        rates = get_rates(base, args.date)
     
     result = convert(amount, to_currency, rates)
     
--- easy_converter.py
+++ easy_converter.py
@@ -1,5 +1,4 @@
 import sys
-import json
 import requests
 
 
